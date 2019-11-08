d1fa2f64b650d1ef578b75ef2c78ee44e6de793b
Lucas Germano <lucas.germano@gmail.com>
2019-11-08 00:05:36 -0200

    master

--- src/app/retrospective/retrospective.component.ts
+++ src/app/retrospective/retrospective.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './retrospective.component.html'
+    templateUrl: './retrospective.component.html',
+    styleUrls: ['./retrospective.component.scss']
 })
 export class RetrospectiveComponent {
 }
